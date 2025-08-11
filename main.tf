6e4e507aebf84fe9e4efb8459f6887d98aaf2f40
Madhuri Singhn <singmadhurri@gmail.com>
2025-08-11 16:27:50 +0530

    add rg and stg

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    azurerm = {
+        source = "hashicorp/azurerm"
+        version = "4.38.1"
+
+    }
+  }
+}
+provider "azurerm" {
+  features {}
+    subscription_id = "25a58ceb-2570-42e2-9d35-00b46979c51a"
+  }
+  resource "azurerm_resource_group" "myrg" {
+    name = "merakaamrghai"
+    location = "westus"
+  }
+  resource "azurerm_storage_account" "example" {
+  name                     = "merakaamstghai"
+  resource_group_name      = "merakaamrghai"
+  location                 = "westus"
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+  depends_on = [ azurerm_resource_group.myrg ]
+  }
